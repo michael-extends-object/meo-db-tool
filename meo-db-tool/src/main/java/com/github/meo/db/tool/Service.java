32647104d17c036e9be334e4e3ae2fa5d37ee898
michael-extends-object <michael.extends.object@gmail.com>
2011-11-06 00:41:17 +0100

    Basic Spring Template Project This basic Spring template project will be used as bases for the future development.

---
+++ meo-db-tool/src/main/java/com/github/meo/db/tool/Service.java
@@ -0,0 +1,7 @@
+package com.github.meo.db.tool;
+
+public interface Service {
+	
+	String getMessage();
+
+}
---
+++ meo-db-tool/src/test/java/com/github/meo/db/tool/ExampleServiceTests.java
@@ -0,0 +1,14 @@
+package com.github.meo.db.tool;
+
+import com.github.meo.db.tool.ExampleService;
+import junit.framework.TestCase;
+
+public class ExampleServiceTests extends TestCase {
+
+	private ExampleService service = new ExampleService();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", service.getMessage());
+	}
+
+}
