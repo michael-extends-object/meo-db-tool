3a78b7a27f113539380adddb214162df9b74f561
michael-extends-object <michael.extends.object@gmail.com>
2011-11-27 00:08:11 +0100

    Class DatabaseTable: Added list of columns

--- meo-db-tool/src/main/java/com/github/meo/db/tool/domain/DatabaseTable.java
+++ meo-db-tool/src/main/java/com/github/meo/db/tool/domain/DatabaseTable.java
@@ -1,15 +1,33 @@
 package com.github.meo.db.tool.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseTable {
 
 	private String name;
-
+	private List<DatabaseTableColumn> databaseTableColumns;
+	
 	public DatabaseTable() {
-
+		init();
 	}
 
 	public DatabaseTable(String name) {
+		init();
 		setName(name);
+	}
+
+	private void init() {
+		setDatabaseTableColumns(new ArrayList<DatabaseTableColumn>());
+	}
+
+	public List<DatabaseTableColumn> getDatabaseTableColumns() {
+		return databaseTableColumns;
+	}
+
+	public void setDatabaseTableColumns(
+			List<DatabaseTableColumn> databaseTableColumns) {
+		this.databaseTableColumns = databaseTableColumns;
 	}
 
 	public String getName() {
